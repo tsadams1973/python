75df4ed0a1fc54cb0720e26ec09ad65693ac12be
timothy.adams@covatrix.com <tsa.adams@gmail.com>
2020-02-12 20:55:48 -0500

    python practice with az computer vision

---
+++ hello.py
@@ -0,0 +1,12 @@
+msg = "Hello Python"
+print(msg)
+
+msg.capitalize
+print(msg)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,20,100)
+plt.plot(x, np.sin(x))
+plt.show()
